f95d9b8685cf6f02d9ec4701879af98ef6ded49e
dtolkachev <dtolkachev.04@gmail.com>
2024-03-24 14:20:13 +0300

    podkluchenie k koshelkam main and testnet

--- src/app/koshelok/page.tsx
+++ src/app/koshelok/page.tsx
@@ -1,45 +1,81 @@
 'use client'
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import { ethers } from 'ethers'
+import Web3 from 'web3';
 
 const Page = () => {
 
 const [userAccount, setUserAccount]=useState('')
-const [balance, setBalance]=useState('')
+//const [balance, setBalance]=useState('')
+    //
+    // const onConnected=()=>{
+    // const provider=new ethers.EtherscanProvider
+    //     if(window.ethereum){
+    //         window.ethereum.request({method:'eth_requestAccounts'}).then((acc:any)=>{
+    //             console.log(acc)
+    //             setUserAccount(acc[0])
+    //             onBalance(acc[0])
+    //
+    //         })
+    //             .catch((e:any)=>{
+    //                 console.error(e)
+    //             })
+    //     }
+    //     else{
+    //         alert('скача мету')
+    //     }
+    // }
+    //
+    // useEffect(() => {
+    //     fetch(`https://api.etherscan.io/api?module=account&action=txlistinternal&address=${userAccount}&endblock=2702578&page=1&offset=10&sort=asc&apikey=XI67VVMYANJ7R4S64DNMPKRH4SGDC8H4XW`)
+    //         .then(response => response.json())
+    //         .then(data => {
+    //             console.log(data)
+    //         });
+    // }, [userAccount]);
+    //
+    // const onBalance =  (account:any)=>{
+    //  window.ethereum.request({
+    //     method:'eth_getBalance',
+    //     params: [
+    //         account,
+    //         'latest'
+    //     ]
+    // }).then((res:any)=>{
+    //      setBalance(ethers.formatEther(res))
+    //
+    //  }).catch((e:any)=>{
+    //      console.log(e)
+    //  })
+    // }
 
-    const onConnected=()=>{
-        if(window.ethereum){
-            window.ethereum.request({method:'eth_requestAccounts'}).then((acc:any)=>{
-                console.log(acc)
-                setUserAccount(acc[0])
-                onBalance(acc[0])
+    const provider = window.ethereum;
 
-            })
-                .catch((e:any)=>{
-                    console.error(e)
-                })
+// Initialize web3 instance
+    const web3 = new Web3(provider);
+const kosh = async ()=> {
+// Get currently selected account
+        const accounts = await web3.eth.getAccounts();
+    console.log(accounts)
+// Get network ID to check if correct network
+        const networkId = await web3.eth.net.getId();
+    console.log(networkId)
+// BSC Mainnet network id
+        const targetNetworkId = 56;
+
+// Check network
+        if (networkId !== BigInt(targetNetworkId)) {
+            console.log(networkId)
+            console.log("Wrong network! Switch to BSC");
+            return;
         }
-        else{
-            alert('скача мету')
-        }
+
+// Call contract method to get balance of selected account
+        const balance = await web3.eth.getBalance(accounts[0]);
+
+// Output balance
+        console.log(web3.utils.fromWei(balance, 'ether'), 'BNB')
     }
-
-    const onBalance =  (account:any)=>{
-     window.ethereum.request({
-        method:'eth_getBalance',
-        params: [
-            account,
-            'latest'
-        ]
-    }).then((res:any)=>{
-         setBalance(ethers.formatEther(res))
-
-     }).catch((e:any)=>{
-         console.log(e)
-     })
-    }
-
-
     return (
         <>
           koshelok
@@ -48,13 +84,13 @@ const [balance, setBalance]=useState('')
                     ?(
                         <>
                         <div>ACCOUNT: {userAccount}</div>
-                        <div>Balance: {balance}</div>
+                        <div>Balance: {}</div>
                     </>
                     )
                     :<>111</>
                 }
             </>
-            <button onClick={onConnected}>CONNECT</button>
+            <button onClick={kosh}>CONNECT</button>
         </>
     );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,78 @@
+'use client'
 import Image from "next/image";
-import styles from "./page.module.css";
+import {Inter} from "next/font/google";
+import {Header} from "./../../components/header";
+import {useCallback, useState} from "react";
+import {ethers} from "ethers";
+
+const inter = Inter({subsets: ["latin"]});
+
+export interface AccountType {
+    address?: string;
+    balance?: string;
+    chainId?: string;
+    network?: string;
+}
+
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    const [accountData, setAccountData] = useState<AccountType>({});
+    const _connectToMetaMask = useCallback(async () => {
+        const ethereum = window.ethereum;
+        // Check if MetaMask is installed
+        if (typeof ethereum !== "undefined") {
+            try {
+                // Request access to the user's MetaMask accounts
+                const accounts = await ethereum.request({
+                    method: "eth_requestAccounts",
+                });
+                // Get the connected Ethereum address
+                const address = accounts[0];
+                console.log(address)
+                // Create an ethers.js provider using the injected provider from MetaMask
+                const provider = new ethers.BrowserProvider(ethereum);
+                // Get the account balance
+                const balance = await provider.getBalance(address);
+                // Get the network ID from MetaMask
+                const network = await provider.getNetwork();
+                // Update state with the results
+                setAccountData({
+                    address,
+                    balance: ethers.formatEther(balance),
+                    // The chainId property is a bigint, change to a string
+                    chainId: network.chainId.toString(),
+                    network: network.name,
+                });
+            } catch (error: Error | any) {
+                alert(`Error connecting to MetaMask: ${error?.message ?? error}`);
+            }
+        } else {
+            alert("MetaMask not installed");
+        }
+    }, []);
+
+    return (
+        <div
+            className={`h-full flex flex-col before:from-white after:from-sky-200 py-2 ${inter.className}`}
+        >
+            <Header address={accountData.address} balance={accountData.balance} chainId={accountData.chainId} network={accountData.network}/>
+            <div className="flex flex-col flex-1 justify-center items-center">
+                <div className="grid gap-4">
+                    <Image
+                        src="https://images.ctfassets.net/9sy2a0egs6zh/4zJfzJbG3kTDSk5Wo4RJI1/1b363263141cf629b28155e2625b56c9/mm-logo.svg"
+                        alt="MetaMask"
+                        width={320}
+                        height={140}
+                        priority
+                    />
+                    <button
+                        onClick={_connectToMetaMask}
+                        className="bg-black text-white p-4 rounded-lg"
+                    >
+                        Connect to MetaMask
+                    </button>
+                </div>
+            </div>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    )
 }
