2831b4060fef7742cdb415ff153132026b64f40b
dtolkachev <dtolkachev.04@gmail.com>
2024-03-27 11:09:30 +0300

    desktop vers

---
+++ src/components/wallet.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import * as Tabs from '@radix-ui/react-tabs';
+import './style.css';
+
+const Wallet = () => (
+    <Tabs.Root className="TabsRoot" defaultValue="tab1">
+        <Tabs.List className="TabsList" aria-label="Manage your account">
+            <Tabs.Trigger className="TabsTrigger" value="tab1">
+                Fast send
+            </Tabs.Trigger>
+            <Tabs.Trigger className="TabsTrigger" value="tab2">
+                Fast exchange
+            </Tabs.Trigger>
+        </Tabs.List>
+        <Tabs.Content className="TabsContent" value="tab1">
+            <p className="Text">There you can send some tokens to your friend</p>
+            <div style={{display: 'flex', flexDirection: 'row', }}>
+                <fieldset className="Fieldset" >
+                    <label className="Label" htmlFor="name">
+                        Amount
+                    </label>
+                    <input className="Input" id="name"  type={"number"}/>
+                </fieldset>
+                <fieldset className="Fieldset">
+                    <label className="Label" htmlFor="username">
+                        Adress
+                    </label>
+                    <input className="Input" id="username" />
+                </fieldset>
+            </div>
+            <div style={{display: 'flex', marginTop: 20, justifyContent: 'flex-start'}}>
+                <button className="Button green">Send </button>
+            </div>
+        </Tabs.Content>
+        <Tabs.Content className="TabsContent" value="tab2">
+            <p className="Text">There you can sell some tokens</p>
+            <div style={{display: 'flex', flexDirection: 'row', }}>
+                <fieldset className="Fieldset" >
+                    <label className="Label" htmlFor="name">
+                        Amount
+                    </label>
+                    <input className="Input" id="name" />
+                </fieldset>
+                <fieldset className="Fieldset">
+                    <label className="Label" htmlFor="username">
+                        Currency
+                    </label>
+                    <input className="Input" id="username" />
+                </fieldset>
+            </div>
+            <div style={{display: 'flex', marginTop: 20, justifyContent: 'flex-start'}}>
+                <button className="Button green">Sell</button>
+            </div>
+        </Tabs.Content>
+    </Tabs.Root>
+);
+
+export default Wallet;
